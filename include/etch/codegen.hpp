a4fffa0eec72f16ea09331f95727b1089dcce22f
Ori Sky Farrell <ori.sky.farrell+git@gmail.com>
2021-04-17 17:45:36 +0100

    Stub codegen function

---
+++ include/etch/codegen.hpp
@@ -0,0 +1,9 @@
+#ifndef ETCH_CODEGEN_HPP
+#define ETCH_CODEGEN_HPP 1
+
+namespace etch {
+	void codegen(const analysis::module_ &m) {
+	}
+} // namespace etch
+
+#endif
--- include/etch/compiler.hpp
+++ include/etch/compiler.hpp
@@ -2,13 +2,16 @@
 #define ETCH_COMPILER_HPP 1
 
 #include <etch/analysis/semantics.hpp>
+#include <etch/codegen.hpp>
 #include <etch/parser.hpp>
 #include <string_view>
 
 namespace etch {
 	analysis::module_ compile(std::string_view sv) {
 		auto sm = parse(sv);
-		return analysis::semantics{}.run(sm);
+		auto m = analysis::semantics{}.run(sm);
+		codegen(m);
+		return m;
 	}
 } // namespace etch
 

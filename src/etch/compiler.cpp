55f1382287ea5e74ee325471cf3b99863321b239
Ori Sky Farrell <ori.sky.farrell+git@gmail.com>
2021-04-19 17:55:14 +0100

    Emit machine assembly

--- src/etch/compiler.cpp
+++ src/etch/compiler.cpp
@@ -3,7 +3,13 @@
 #include <etch/compiler.hpp>
 #include <etch/parser.hpp>
 #include <etch/transform/fold.hpp>
+#include <llvm/IR/LegacyPassManager.h>
 #include <llvm/IR/Module.h>
+#include <llvm/Support/Host.h>
+#include <llvm/Support/TargetRegistry.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Target/TargetOptions.h>
+#include <llvm/Target/TargetMachine.h>
 
 namespace etch {
 	std::string etch::compile(std::string_view sv, std::string name) {
@@ -20,11 +26,46 @@ namespace etch {
 		llvm::verifyModule(*m, &llvm::errs());
 
 		// output LLVM assembly to string
+
 		std::string ll;
 		llvm::raw_string_ostream os(ll);
 		os << *m;
 		os.flush();
 
-		return ll;
+		// emit object
+
+		llvm::InitializeAllTargetInfos();
+		llvm::InitializeAllTargets();
+		llvm::InitializeAllTargetMCs();
+		llvm::InitializeAllAsmPrinters();
+		llvm::InitializeAllAsmParsers();
+
+		auto triple = llvm::sys::getDefaultTargetTriple();
+		m->setTargetTriple(triple);
+
+		std::cout << "target triple = " << triple << std::endl;
+
+		std::string err;
+		auto target = llvm::TargetRegistry::lookupTarget(triple, err);
+
+		std::string cpu = "generic";
+		std::string features = "";
+		llvm::TargetOptions opts;
+		auto target_machine = target->createTargetMachine(triple, cpu, features, opts, {});
+
+		m->setDataLayout(target_machine->createDataLayout());
+
+		llvm::SmallString<256> assembly;
+		llvm::raw_svector_ostream s_asm(assembly);
+
+		llvm::legacy::PassManager pm;
+		auto ft = llvm::CGFT_AssemblyFile;
+
+		if(target_machine->addPassesToEmitFile(pm, s_asm, nullptr, ft)) {
+			std::cerr << "ERROR: cannot emit file type" << std::endl;
+		}
+		pm.run(*m);
+
+		return std::string(assembly);
 	}
 } // namespace etch

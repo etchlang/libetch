53bc55b4509c7c9071acb8bbd37a81e5d7fbd26d
Ori Sky Farrell <ori.sky.farrell+git@gmail.com>
2021-04-27 05:41:39 +0100

    semantics: Clean up returns a bit

--- include/etch/analysis/semantics.hpp
+++ include/etch/analysis/semantics.hpp
@@ -10,66 +10,65 @@ namespace etch::analysis {
 	class semantics {
 	  public:
 		template<typename T>
-		value::ptr visit(const syntax::typed<T> &x) {
+		auto visit(const syntax::typed<T> &x) {
 			auto val = visit(x.value);
 			auto ty  = visit(x.type);
 			return std::make_shared<value::cast>(val, ty);
 		}
 
 		template<typename T>
-		value::ptr visit(const syntax::x3::forward_ast<syntax::typed<T>> &x) {
+		auto visit(const syntax::x3::forward_ast<syntax::typed<T>> &x) {
 			return visit(x.get());
 		}
 
 		template<typename... Ts>
 		value::ptr visit(const syntax::x3::variant<Ts...> &x) {
 			return boost::apply_visitor([this](auto &&sv) {
-				return visit(sv);
+				return std::static_pointer_cast<value::base>(visit(sv));
 			}, x);
 		}
 
-		value::ptr visit(const syntax::block &sb) {
+		auto visit(const syntax::block &sb) {
 			auto b = std::make_shared<value::block>();
 			for(auto &x : sb) {
 				b->push_back(visit(x));
 			}
-			return std::static_pointer_cast<value::base>(b);
+			return b;
 		}
 
-		value::ptr visit(const syntax::tuple &st) {
+		auto visit(const syntax::tuple &st) {
 			auto t = std::make_shared<value::tuple>();
 			for(auto &x : st) {
 				t->push_back(visit(x));
 			}
-			return std::static_pointer_cast<value::base>(t);
+			return t;
 		}
 
-		value::ptr visit(const syntax::identifier &x) {
-			return std::static_pointer_cast<value::base>(std::make_shared<value::identifier>(x));
+		auto visit(const syntax::identifier &x) {
+			return std::make_shared<value::identifier>(x);
 		}
 
-		value::ptr visit(const syntax::intrinsic &x) {
-			return std::static_pointer_cast<value::base>(std::make_shared<value::intrinsic>(x));
+		auto visit(const syntax::intrinsic &x) {
+			return std::make_shared<value::intrinsic>(x);
 		}
 
-		value::ptr visit(const syntax::integer &i) {
-			return std::static_pointer_cast<value::base>(std::make_shared<value::constant_int>(i.value));
+		auto visit(const syntax::integer &i) {
+			return std::make_shared<value::constant_int>(i.value);
 		}
 
-		value::ptr visit(const syntax::function &sf) {
+		auto visit(const syntax::function &sf) {
 			auto arg = visit(sf.arg);
 			auto body = visit(sf.body);
-			auto f = std::make_shared<value::function>(arg, body);
-			return std::static_pointer_cast<value::base>(f);
+			return std::make_shared<value::function>(arg, body);
 		}
 
-		value::ptr visit(const syntax::definition &sd) {
+		auto visit(const syntax::definition &sd) {
 			auto binding = visit(sd.binding);
 			auto val = visit(sd.value);
-			return std::static_pointer_cast<value::base>(std::make_shared<value::definition>(binding, val));
+			return std::make_shared<value::definition>(binding, val);
 		}
 
-		value::ptr visit(const syntax::op &so) {
+		auto visit(const syntax::op &so) {
 			std::shared_ptr<value::call> c;
 
 			auto lhs = visit(so.lhs);
@@ -85,10 +84,10 @@ namespace etch::analysis {
 				c = std::make_shared<value::call>(visit(so.opname), t);
 			}
 
-			return std::static_pointer_cast<value::base>(c);
+			return c;
 		}
 
-		std::shared_ptr<value::module_> visit(const syntax::module &sm) {
+		auto visit(const syntax::module &sm) {
 			auto m = std::make_shared<value::module_>();
 			for(auto &st : sm) {
 				m->defs.emplace_back(visit(st));
